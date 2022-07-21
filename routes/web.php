8647d1ed31eb2179f55a42b4c302fc2464966dad
haohann <haniapn@webmail.umm.ac.id>
2022-07-21 19:29:44 +0700

    add register

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/presensi');
+    return view('/login-pegawai');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
 Route::get('/login', function () {
     return view('login-pegawai');
 });
