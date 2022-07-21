75834498ea3368ec89bbf2d37dbbbe2474b0f2a2
ARSTCreations <anandarizaldyaristyo@gmail.com>
2022-07-21 20:12:48 +0700

    Fixed Root View

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/presensi');
+    return redirect('/dashboard');
 });
 
 Route::get('/login', function () {
