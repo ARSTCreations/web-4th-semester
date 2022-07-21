3975d3732bf8283b2b012568f799e102fe5c83be
haohann <haniapn@webmail.umm.ac.id>
2022-07-21 11:03:50 +0700

    Update agenda.blade.php

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -2,10 +2,14 @@
 @section('css')
 <link rel="stylesheet" href="{{asset('css/index.css')}}">
 <link rel="stylesheet" href="{{asset('css/agenda.css')}}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
 @stop
 @section('content')
 <header>
-    <div class="box-daftar-agenda">
+<div class="box-daftar-agenda">
         <h1>Daftar Agenda</h1>
         <div class="card-agenda">
             <p>Selasa, 19 Juli 2022</p>
@@ -17,6 +21,66 @@
     </div>
     <div class="box-kalender-agenda">
         <h1>Kalender Agenda</h1>
+        <br />
+        <div class="container">
+        <br />
+    <br />
+    <div id="calendar"></div>
+
+</div>
+   
+<script>
+
+$(document).ready(function () {
+
+    $.ajaxSetup({
+        headers:{
+            'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    var calendar = $('#calendar').fullCalendar({
+        editable:true,
+        header:{
+            left:'prev,next today',
+            center:'title',
+            right:'month'
+        },
+        events:'/full-calender',
+        selectable:true,
+        selectHelper: true,
+        select:function(start, end, allDay)
+        {
+            var title = prompt('Event Title:');
+
+            if(title)
+            {
+                var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
+
+                var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
+
+                $.ajax({
+                    url:"/full-calender/action",
+                    type:"POST",
+                    data:{
+                        title: title,
+                        start: start,
+                        end: end,
+                        type: 'add'
+                    },
+                    success:function(data)
+                    {
+                        calendar.fullCalendar('refetchEvents');
+                        alert("Event Created Successfully");
+                    }
+                })
+            }
+        }
+    });
+
+});
+  
+</script>
     </div>
     
 </header>
